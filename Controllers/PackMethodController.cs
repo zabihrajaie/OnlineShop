1d83e7db274528b9ee77ba88a90cb613ec0ee177
zabih rajaie <zabih.rajaie@home>
2021-12-17 00:04:20 +0330

    Controllers

---
+++ Controllers/PackMethodController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Domain.Services;
+
+namespace OnlineShop.Controllers
+{
+    public class SendTypeController : BaseController
+    {
+        private readonly ISendTypeService _sendTypeService;
+
+        public SendTypeController(ISendTypeService sendTypeService)
+        {
+            _sendTypeService = sendTypeService;
+        }
+
+        // GET
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Controllers/SendTypeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Domain.Services;
+
+namespace OnlineShop.Controllers
+{
+    public class PackMethodController : BaseController
+    {
+        private readonly IPackMethodService _packMethodService;
+
+        public PackMethodController(IPackMethodService packMethodService)
+        {
+            _packMethodService = packMethodService;
+        }
+
+        // GET
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
