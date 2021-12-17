7c5079c99974502aa685d764df32b5079d33318c
zabih rajaie <zabih.rajaie@home>
2021-12-17 11:57:58 +0330

    User

---
+++ Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using OnlineShop.Domain.Dtos.User;
+using OnlineShop.Domain.Services;
+
+namespace OnlineShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery] UsersFilterDto dto)
+        {
+            return Ok(await _userService.GetAll(dto));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(UserAddDto dto)
+        {
+            await _userService.AddAsync(dto);
+            return Created(nameof(Post), dto);
+        }
+    }
+}
